398d5a757adc7922489ffa6b87eff1a8eabcfe33
LORENZO-Z77\Lorenzo_Z77 <lguidaldi@outlook.com>
2017-03-10 11:39:44 +0100

    /IMPORTANTE/ Ho svuotato la repository per preparare il nuovo master /////////////////////////////////////////////////////////////////////////////

--- BadRock alpha 0.10/myData.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-M.maxLevels = 5
-M.settings = {}
-M.settings.currentLevel = 1
-M.settings.unlockedLevels = 2
-M.settings.soundOn = true
-M.settings.musicOn = true
-M.settings.levels = {} 
-return M
