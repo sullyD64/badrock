644dc5f4a7899ba51c7595af47fa71a990b04348
Giacomo Bonanni <Giacomo Bonanni>
2017-05-07 20:16:47 +0200

    refactor movimento nemici

--- BADROCK NEW REPO/core/enemies.lua
+++ BADROCK NEW REPO/core/enemies.lua
@@ -10,13 +10,142 @@ local panel  = require ( "menu.utilityMenu" )
 
 local enemies = {}
 
+
+	function follow(currentGame,object,player)
+			local currentMap = currentGame.map
+			if((object.x~=nil and object.y~=nil) and(player.x~=nil and player.y~=nil) and currentGame.state~="Paused") then--il problema è che poi non richiama la transizione del disaggro
+																															--per colpa del game.paused quando steve muore
+				object.isFixedRotation=true
+				object.speed=1--quello sopra continua a essere flash
+				if(math.abs(object.x-player.x)<=400) then
+					local angle= math.atan2(player.y - object.y, player.x - object.x) -- work out angle between target and missile
+					object.x = object.x + (math.cos(angle) * object.speed) -- update x pos in relation to angle
+						if(player.x>object.x) then
+						object.xScale=-1
+						else object.xScale=1
+						end
+						--se steve è sopra una piattaforma non lontana dal nemico allora il nemico salta, non funziona
+						if(((object.y-player.y)>0 and (object.y-player.y)<1000) and (math.abs(player.x-object.x)<40) ) then
+							local impulso= (player.y-object.y)
+							object:applyLinearImpulse( 0, -impulso*4, object.x, object.y )
+						end
+				
+					local vuoto = nil
+					local vuotoList = currentMap:getObjectLayer("cadutaVuoto").objects
+
+						for k, v in pairs(vuotoList) do
+							vuoto = vuotoList[k]
+
+						end
+					local direzione=math.abs(object.x-vuoto.x)
+					local distanzab= math.abs(direzione)
+					local distanzaverticalebordo= math.abs(object.y-vuoto.y)
+
+
+					--i nemici disaggrano steve alla morte, bisogna sistemare il momento in cui lo considerano morto? sgommata se si rimane fermi...
+						if(((player.state == STATE_DIED and currentGame.state=="Running"))) then --and (math.abs(object.x-player.x)<=100 and math.abs(object.y-player.y)<=100)) ) then
+
+							object.xScale=-1
+							
+							local transition= transition.to(object,{time=3500,xScale=-1,x=(object.x+280)})--onPause= function() print ("transition paused")end})
+						end
+						--dovrebbe disaggrare anche se si trova nei pressi dello spawn point, per evitare che steve spawni dove c'è un nemico
+						-- if (math.abs(currentGame.spawn.x-object.x)<=150 and (currentGame.spawn.y==object.y)) then
+						-- 	object.xScale=-1
+							
+						-- 	local transition= transition.to(object,{time=3500,xScale=-1,x=(object.x+280)})
+						-- end
+
+						--saltano continuamente a prescindere
+						local salto=false
+						if(distanzab<=100 and distanzaverticalebordo<=100 and object.lives~=0 ) then
+							
+							if(object.xScale==-1 and salto==false) then
+							object:applyLinearImpulse( 5, -15, object.x, object.y )
+							salto=true
+							elseif(object.xScale==1 and salto==false) then
+							object:applyLinearImpulse( -5, -15, object.x, object.y )
+							salto=true
+							end
+
+						end
+					
+					
+				end
+			end
+	end
+
+
+
+------------------------------------------------------------------------------------
+
+-- GIGI WIP-------------------------------------------------------------------------
+	--[[
+		function prova()
+		for k,v in ipairs(game.enemyLevelList) do
+		transition.to(game.enemyLevelList[k], {
+				time=1500,
+				x=(game.enemyLevelList[k].x - 120),
+				onComplete=prova()
+			})
+		end
+	]]
+
+	function f(object)
+		local angle= math.atan2(game.steve.x - object.y, game.steve.y - object.x) -- work out angle between target and missile
+		object.x = object.x + (math.cos(angle) * object.speed) -- update x pos in relation to angle
+		object.y = object.y + (math.sin(angle) * object.speed) -- update y pos in relation to angle
+	end
+
+
+	-- i nemici si muovono a destra e sinistra, lista
+	function muovi(object)
+		object.isFixedRotation=true
+		function goLeft ()
+		transition.to( object, { time=1500, x=(object.x - 120), onComplete=goRight } )
+		object.xScale=1
+		end
+
+		function goRight ()
+		transition.to( object, { time=1500, x=(object.x + 120), onComplete=goLeft } )
+		object.xScale=-1
+		end
+
+		goLeft()
+	end
+
+
+
+	-- i nemici dovrebbero seguire steve, per alcuni, liste
+	function segui(steve)
+		for i = 1, #enemiesList do
+			local distanceX = steve.x - enemiesList[i].x
+			local distanceY = steve.y - enemiesList[i].y
+
+			local angleRadians = math.atan2(distanceY, distanceX)
+			local angleDegrees = math.deg(angleRadians)
+
+			local enemySpeed = 5
+
+			local enemyMoveDistanceX = enemySpeed*math.cos(angleDegrees)
+			local enemyMoveDistanceY = enemySpeed*math.sin(angleDegrees)
+
+			enemy.x = enemy.x + enemyMoveDistanceX
+			enemy.y = enemy.y + enemyMoveDistanceY
+		end
+	end
+
+	
+-- Loads the enemies's images, speech balloons and initializes their attributes.
+
 -- Loads the enemies's images (and sprites) and initializes their attributes.
+
 -- Visually instantiates the enemies in the current game's map.
 -- @return enemies (a table of enemies)
 function enemies.loadEnemies( currentGame ) 
 	local currentMap = currentGame.map
 	local enemyList = currentMap:getObjectLayer("enemySpawn"):getObjects("enemy")
-	local imageList= {}
+	local player= currentGame.steve
 
 	-- Loads the main Entity.
 		local loadenemyEntity = function( enemy )
@@ -37,6 +166,15 @@ function enemies.loadEnemies( currentGame )
 				staticImage.lives=1
 				staticImage.x, staticImage.y = enemyList[i].x, enemyList[i].y
 				
+
+				local function move()
+    			follow(currentGame,staticImage,player)
+				end
+		
+				Runtime:addEventListener( "enterFrame", move )
+ 				
+
+
 				elseif (v.type == "sedia") then
  				staticImage = entity.newEntity{
 				graphicType = "static",
@@ -66,7 +204,7 @@ function enemies.loadEnemies( currentGame )
 
 	--la scansione del ciclo dei nemici in tutta la mappa è fatta all'interno di loadenemy
 	enemyList[1].staticImage = loadenemyEntity(enemyList[1])
-
+	
 	return enemyList
 end
 
