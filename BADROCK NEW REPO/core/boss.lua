f4f5a5ae8875a586ff1b931af61c1fbde96bd004
IronAxe (Portatile) <c.fabio94@gmail.com>
2017-06-02 12:50:04 +0200

    Creazione della classe Boss che conterrà metodi e variabili per la gestione e creazione di un boss

---
+++ BADROCK NEW REPO/core/boss.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+-- BOSS is a class used to store any Boss actions and methods, can create a boss with specified actions and variables
+
+-----------------------------------------------------------------------------------------
+
+local boss = {}
+
+
+
+return boss
