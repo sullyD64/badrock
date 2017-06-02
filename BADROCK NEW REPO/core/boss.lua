aadaedffccf33b595da5793c0a0c6071401c000a
Giacomo Bonanni <Giacomo Bonanni>
2017-06-02 16:03:12 +0200

    funzioni parametriche di boss [da controllare]

--- BADROCK NEW REPO/core/boss.lua
+++ BADROCK NEW REPO/core/boss.lua
@@ -142,15 +142,84 @@ local enemies = {
 	return bossEntity
 	end
 
-	--il boss si presenta
+	--il boss ha n vite
+	function gestisciVita(n)
+	local lifeBar = {}
+	local lives = n
+	local x=10
 
-	function presentati()
+		for i=1,n do
+			lifeBar[i] = display.newImage("nomeImmagine.png")
+			lifeBar[i].x = x+10
+			lifeBar[i].y = 30
+			lifeBar[i].isVisible = true
+		end
 
+
+		--aggiorna
+		if lives>0 then
+		    lifeBar[lives].isVisible = false
+		    lives = lives - 1
+		end
 	end
 
-	-- lanciatore spara un oggettoLanciato contro target
-	function spara(lanciatore,oggettoLanciato,target)
+-- local function textListener( event )
+ 
+--     if ( event.phase == "began" ) then
+        
+ 
+--     elseif ( event.phase == "ended" or event.phase == "submitted" ) then
+            
+--     end
+-- end
 
+--inizia un breve dialogo con il nemico, testo da mettere in un array per poterne scorrere gli elementi
+function presentati()
+-- defaultBox = native.newTextBox( display.contentCenterX, display.contentCenterY, 280, 140 )
+-- defaultBox.text = "Well.. I didn't expect you would have survived"
+local textField = native.newTextField( display.contentCenterX, display.contentCenterY, 260, 100 )
+textField:setTextColor( 0.8, 0.8, 0.8 )
+textField.size=15
+textField.text="Well.. I didn't expect you would have survived"
+--background del textbox trasparente, non supportato da emulatore su windows
+textField.hasBackground = false
+--cliccando si scorre il testo più velocemente
+--defaultBox:addEventListener( "touch", textListener )
+end
+
+
+	-- lanciatore spara un objectToThrow contro target
+	function spara(lanciatore,objectToThrow,target)
+
+	local function creaOggetto(creazione,lanciatore)
+					creazione = entity.newEntity{
+					graphicType = "static",
+					filePath = visual.npcImage,
+					--width = 50,
+					--height = 37,
+					bodyType = "dynamic",
+					physicsParams = { isSensor=true },
+					--eName = "creazione"
+				}
+				creazione.isEnemy=true
+				--creazione.xScale=-1
+				--creazione.yScale=-1
+				--creazione.rotation=90
+				--creazione.gravityScale=0.025
+				creazione.x, creazione.y = lanciatore.x, lanciatore.y
+				creazione.isBreakable= true
+				return creazione
+	end
+	if(target.x and lanciatore.x and target.y and lanciatore.y) then
+	local fx= target.x-lanciatore.x
+	local fy= target.y-lanciatore.y
+	local d=100
+	local den= 80*d
+	creazione= creaOggetto(objectToThrow,lanciatore)
+	creazione.x= creazione.x-100
+	creazione:applyLinearImpulse(fx/den,fy/den,creazione.x,creazione.y)
+	-- if (creazione.y== target.y) then print("fireball e target stessa x") creazione.isSensor=false creazione.bounce=1 else print("no fireball") print(target.y) end
+	-- end
 	end
 
 	--spara un laser in posizione x e y -- FABIO
@@ -158,13 +227,29 @@ local enemies = {
 
 	end
 
-	--le mani inseguono steve
-	function insegui(oggetto, target)
-	transition.to(oggetto,{})
-	end
+	--le mani: 1. si alzano in volo, 2. cercano steve, 3. schiacciano; ripetono i passi 1, 2, 3
 
-	function alzaSchiaccia(oggetto,target)
 
+	function boss.alzaSchiaccia(oggetto,target)
+
+		local function alzati()
+		transition.to(oggetto,{time=1500, y=oggetto.y + 100, onComplete= function() insegui() end})
+		end
+
+		function ricerca()
+			oggetto.x=game.steve.x
+		end
+		local function insegui()
+		Runtime:addEventlListener("enterframe",ricerca)
+		timer.performWithDelay(5000,schiaccia)
+		end
+
+		local function schiaccia()
+		Runtime:removeEventListener("enterframe", ricerca)
+		transition.to(oggetto,{time=500, y=target.y)
+		timer.performWithDelay(2000,alzati)
+		end
+		alzati()
 	end
 
 	return boss
