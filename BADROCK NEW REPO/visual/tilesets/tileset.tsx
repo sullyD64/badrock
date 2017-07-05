6f29c9f72db9862419790bdb0604acb872c31647
Macbook Pro di Elena <mariaelenamadon@hotmail.it>
2017-07-05 20:35:41 +0200

    Aggiunta nuova mappa con relativo tileset e spazio per il boss finale.

---
+++ BADROCK NEW REPO/visual/tilesets/tileset.tsx
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="../badrock/BADROCK NEW REPO/visual/tilesets/tileset.png" width="256" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isPlatform" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isPlatform" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isPlatform" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="tName" value="danger"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isSlippery" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isSlippery" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="isGround" value=""/>
+   <property name="isSlippery" value=""/>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="tName" value="env"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="isSensor" value="true"/>
+   <property name="tName" value="endLevel"/>
+  </properties>
+ </tile>
+</tileset>
