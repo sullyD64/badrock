ad20ab3827353098effc440c5d4d768ae1f01603
Zefyrus94 <giacomo.bonanni94@gmail.com>
2017-07-26 10:18:58 +0200

    copie speculari di level 1 con mappa livello 3 (bossMap) e mappa di appoggio (da sostituire) livello 2

---
+++ BADROCK NEW REPO/levels/level3.lua
@@ -0,0 +1,117 @@
+-----------------------------------------------------------------------------------------
+--
+-- level1.lua
+--
+-----------------------------------------------------------------------------------------
+local composer = require ( "composer"  )
+local tutorial = require ( "tutorial"  )
+local sfx      = require ( "audio.sfx" )
+local game     = require ( "core.game" )
+local loader   = require ( "lib.mapLoader" )
+
+local scene = composer.newScene()
+
+-- -----------------------------------------------------------------------------------
+-- SCENE-ACCESSIBLE CODE
+-- -----------------------------------------------------------------------------------
+
+local mapName, map
+
+local function startTutorial()
+	game.state = "Paused"
+	tutorial.start()
+end
+
+-- -----------------------------------------------------------------------------------
+-- SCENE EVENT FUNCTIONS
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+	local sceneGroup = self.view
+
+	if myData.settings.musicOn == false then
+		audio.play(sfx.bgLvlMusic, {channel = 1 , loops=-1})
+		-- sfx.playMusic(sfx.bgLvlMusic, {channel = 1, loops = -1}) ?
+		audio.pause(1)
+	end
+
+	local blackScreen = display.newRect(display.contentCenterX, display.contentCenterY, 500, 500)
+	blackScreen:setFillColor( 0, 0, 0 ) 
+	sceneGroup:insert(blackScreen)
+
+	local loadingText = display.newText( "LOADING...", display.contentCenterX, display.contentCenterY, native.systemFont, 24 )
+	loadingText:setTextColor( 255, 255, 255 ) 
+	sceneGroup:insert(loadingText)
+
+	 mapName = "bossTest_HD.tmx" 
+	-- mapName = "mapTest_HD.tmx" 
+	--mapName = "level1_DEF - Copia.tmx" 
+end
+
+-- show()
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if (phase == "will") then
+		sfx.playMusic(sfx.bgLvlMusic, {channel = 1 , loops=-1})
+		-- A second audio source is started too but with no volume.
+		-- (needed for syncing the two when we want to swap them)
+		sfx.playMusic(sfx.bgLvlMusicUP, {channel = 8, loops=-1})
+	elseif (phase == "did") then
+		timer.performWithDelay(250, 
+			function()
+				map = loader.loadMap(mapName, sceneGroup)
+				-- La mappa caricata deve SEMPRE avere un layer di OGGETTI chiamato
+				-- checkPoints contenente ALMENO un oggetto "check0" (primo checkPoint)
+				game.loadGame( map, map:getObjectLayer("checkPoints"):getObject("check0"))
+
+				--------------------------------------------------------------------------------
+				-- Commento: in fase di produzione (skippo il mainMenu) può capitare che, se
+				-- completiamo il livello e lo rigiochiamo, il currentLevel sarà aggiornato al 
+				-- livello 2. Quando andrò a fare retry da livello 1, il gioco proverà a caricare
+				-- level2 invece che ricaricare level1.
+				-- Qui sotto forzo myData a level1, andrà tolto prima del deploy dell'app.
+				-- if (myData.settings.currentLevel ~= 1) then myData.settings.currentLevel = 1 end
+				--------------------------------------------------------------------------------
+
+				game.start()
+
+				if (myData.firstStart) then
+					tutorial.create(game)
+					timer.performWithDelay(400, startTutorial)
+				end
+			end
+		)
+	end
+
+end
+
+-- hide()
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	if (phase == "will") then
+		audio.fadeOut(1,10)
+	elseif (phase == "did") then
+	end
+end
+
+-- destroy()
+function scene:destroy( event )
+	local sceneGroup = self.view
+	audio.dispose()
+	package.loaded[game] = nil
+end
+
+-- -----------------------------------------------------------------------------------
+-- SCENE EVENT FUNCTION LISTENERS
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+--------------------------------------------------------------------------------------
+
+return scene
